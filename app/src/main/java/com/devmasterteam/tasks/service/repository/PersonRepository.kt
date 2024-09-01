4f06c3a928371fafe8e37036cf3b5ab2fbd830d3
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-09-01 16:22:39 -0300

    chamada pra api login

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -1,12 +1,26 @@
 package com.devmasterteam.tasks.service.repository
 
+import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.remote.PersonService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PersonRepository {
-    val service = RetrofitClient.createService(PersonService::class.java)
+    private val remote = RetrofitClient.createService(PersonService::class.java)
 
     fun login(email: String, password: String) {
+        val call = remote.login(email, password)
 
+        call.enqueue(object : Callback<PersonModel> {
+            override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
+                val s = response
+            }
+
+            override fun onFailure(call: Call<PersonModel>, t: Throwable) {
+                val s = t
+            }
+        })
     }
 }
--- app/src/main/java/com/devmasterteam/tasks/service/repository/remote/PersonService.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/PersonService.kt
@@ -7,11 +7,14 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface PersonService {
-    @POST("/Authentication/Login")
+    @POST("Authentication/Login")
     @FormUrlEncoded
-    fun login(@Field("email") email: String, @Field("password") password: String): Call<PersonModel>
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<PersonModel>
 
-    @POST("/Authentication/Create")
+    @POST("Authentication/Create")
     @FormUrlEncoded
     fun create(
         @Field("name") name: String,
